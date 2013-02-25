ee48de42fddc380f3f9315cf65c1947f9f7cc0c5
James Whitbeck <james.b.whitbeck@gmail.com>
2013-02-25 15:16:37 -0800

    opts

--- ImageCleaner.cc
+++ ImageCleaner.cc
@@ -292,7 +292,7 @@ void fourier_dit(float *real, float *imag, int size, short *rev, bool invert, fl
 
 void fourier_dif(float *real, float *imag, int size, short *rev, bool invert, float *roots_real, float *roots_imag)
 {
-  for (int span = size >> 1, int num_units = 1; span; span >>= 1, num_units <<= 1)
+  for (int span = size >> 1, num_units = 1; span; span >>= 1, num_units <<= 1)
   {
     for (int unit = 0, two_unit_span = 0; unit < num_units; ++unit, two_unit_span += (span << 1))
     {
